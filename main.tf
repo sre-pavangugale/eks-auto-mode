1b6b154750baa77c3e970dd55f9ffdb36d2a5651
sre-pavangugale <devops.pavangugale@gmail.com>
2026-01-09 15:02:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
+  version         = "21.11.0"
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
   subnet_ids      = module.vpc.private_subnets
