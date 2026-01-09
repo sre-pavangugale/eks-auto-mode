ba0fc17f829026534798a1a28effa562e2b6a1e8
sre-pavangugale <devops.pavangugale@gmail.com>
2026-01-09 15:00:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "kubernetes_version" {
-  default     = 1.27
+  default     = 1.34
   description = "kubernetes version"
 }
 
@@ -8,7 +8,7 @@ variable "vpc_cidr" {
   description = "default CIDR range of the VPC"
 }
 variable "aws_region" {
-  default = "us-west-1"
+  default = "ap-south-1"
   description = "aws region"
 }
 
